939ac7f354f97d2c8632f3fd78665a5d65ac81f3
M1R1 <92366607+MoneRai@users.noreply.github.com>
2024-03-26 19:23:38 +1000

    Update conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+import sys, os
+
+intersphinx_mapping = {
+  'py': ('https://docs.python.org/3', None),
+  'aio': ('https://docs.aiohttp.org/en/stable/', None)
+}
+
+extlinks = {
+    'issue': ('https://github.com/MoneRai/Discord-Selfbot-Framework/issues/%s', 'GH-%s')
+}
+
+source_suffix = '.rst'
+project = 'Discord-Selfbot-Framework'
+version = '1.0.0'
